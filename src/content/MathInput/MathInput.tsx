d7c3202181bddc73200a6d93a52db28062bcb1bf
Deniz Gunay <andacg1@hotmail.com>
2026-02-28 16:28:20 -0800

    added asciimath-to-latex

--- src/content/MathInput/MathInput.tsx
+++ src/content/MathInput/MathInput.tsx
@@ -14,22 +14,30 @@ const MathInput = ({
 }: MathInputProps) => {
     const [latex, setLatex] = useState("\\frac{1}{\\sqrt{2}}\\cdot 2");
     const [container, setContainer] = useState<HTMLElement | null>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         const formSelector = ".form-control[type='text']";
 
         const handleFocus = (event: FocusEvent) => {
-            const target = event.target as HTMLElement;
-            if (target.matches(formSelector)) {
+            const target = event.target as HTMLInputElement;
+            const relatedTarget = event.relatedTarget as HTMLElement;
+            console.log("focus", container, relatedTarget);
+            if (target.matches(formSelector) && !container?.contains(target)) {
                 setContainer(target.parentElement);
+                setLatex(target.value);
             }
         };
 
         const handleBlur = (event: FocusEvent) => {
             const target = event.target as HTMLElement;
-            if (target.matches(formSelector) && event.relatedTarget) {
-                setContainer(null);
+            const relatedTarget = event.relatedTarget as HTMLElement;
+            console.log(container, relatedTarget);
+
+            if (!container?.contains(relatedTarget) || !relatedTarget) {
+                // setContainer(null);
             }
+
         };
 
         document.addEventListener('focusin', handleFocus, true);
@@ -39,20 +47,34 @@ const MathInput = ({
             document.removeEventListener('focusin', handleFocus, true);
             document.removeEventListener('focusout', handleBlur, true);
         };
-    }, []);
+    }, [container]);
 
     if (!container) return null;
 
     return (
       <Portal container={container}>
-          <div className={s.mathInput}>
+          <div className={s.mathInput}
+               id="math-input-container"
+               ref={containerRef}
+               style={{
+                   position: "absolute",
+                   zIndex: 1000,
+                   top: "100%",
+                   backgroundColor: "white"
+               }}>
               <EditableMathField
+                mathquillDidMount={(mathField) => {
+                    console.log("MathField mounted", mathField);
+                    mathField.focus();
+                }}
                 latex={latex}
                 onChange={(mathField) => {
                     setLatex(mathField.latex());
                 }}
+
+                autoFocus={true}
               />
-              <p>{latex}</p>
+              {/* <p>{latex}</p> */}
           </div>
       </Portal>
     );
