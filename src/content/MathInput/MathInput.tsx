af0f3ebf89d66ece86a8fb795826c39d4feb1bb8
Deniz Gunay <andacg1@hotmail.com>
2026-02-28 16:58:47 -0800

    editing latex now updates the input value

--- src/content/MathInput/MathInput.tsx
+++ src/content/MathInput/MathInput.tsx
@@ -1,7 +1,9 @@
-import { type ReactNode, useEffect, useRef, useState } from "react";
+import { type ReactNode, useCallback, useEffect, useRef, useState } from "react";
+import { MQtoAM } from "./AMtoMQ.ts";
 import s from "./MathInput.module.css";
 import Portal from "@mui/material/Portal";
 import { addStyles, EditableMathField } from "react-mathquill";
+import asciiTolatex from 'asciimath-to-latex'
 
 addStyles();
 
@@ -9,33 +11,36 @@ type MathInputProps = {
     children?: ReactNode
 }
 
+
 const MathInput = ({
     children
 }: MathInputProps) => {
+    const formSelector = ".form-control[type='text']";
     const [latex, setLatex] = useState("\\frac{1}{\\sqrt{2}}\\cdot 2");
     const [container, setContainer] = useState<HTMLElement | null>(null);
     const containerRef = useRef<HTMLDivElement>(null);
+    const updateValue = useCallback((newValue: string) => {
+        (container?.querySelector(formSelector) as HTMLInputElement).value = MQtoAM(newValue, true);
+    }, [container])
 
     useEffect(() => {
-        const formSelector = ".form-control[type='text']";
-
         const handleFocus = (event: FocusEvent) => {
             const target = event.target as HTMLInputElement;
             const relatedTarget = event.relatedTarget as HTMLElement;
-            console.log("focus", container, relatedTarget);
+            // console.log("focus", container, relatedTarget);
             if (target.matches(formSelector) && !container?.contains(target)) {
                 setContainer(target.parentElement);
-                setLatex(target.value);
+                setLatex(asciiTolatex(target.value));
             }
         };
 
         const handleBlur = (event: FocusEvent) => {
             const target = event.target as HTMLElement;
             const relatedTarget = event.relatedTarget as HTMLElement;
-            console.log(container, relatedTarget);
+            // console.log(container, relatedTarget);
 
             if (!container?.contains(relatedTarget) || !relatedTarget) {
-                // setContainer(null);
+                setContainer(null);
             }
 
         };
@@ -60,21 +65,23 @@ const MathInput = ({
                    position: "absolute",
                    zIndex: 1000,
                    top: "100%",
-                   backgroundColor: "white"
+                   backgroundColor: "white",
+                   padding: "0.2em"
                }}>
               <EditableMathField
                 mathquillDidMount={(mathField) => {
-                    console.log("MathField mounted", mathField);
+                    // console.log("MathField mounted", mathField);
                     mathField.focus();
+                    Object.assign(mathField.el().style, {padding: "0.5em"})
                 }}
                 latex={latex}
                 onChange={(mathField) => {
                     setLatex(mathField.latex());
+                    updateValue(mathField.latex());
                 }}
 
                 autoFocus={true}
               />
-              {/* <p>{latex}</p> */}
           </div>
       </Portal>
     );
